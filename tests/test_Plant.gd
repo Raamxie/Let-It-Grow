4db7c5c1f4b393756ac536e0900b558a7dd201c2
SasaSnidova <s.snidova@gmail.com>
2025-05-20 00:42:01 +0200

    start of UI

---
+++ tests/test_Plant.gd
@@ -0,0 +1,6 @@
+extends "res://addons/gut/test.gd"
+
+class_name PlantTest
+
+func test_plaaant():
+	assert_eq("yaaaay", "yaaaay")
---
+++ tests/test_player.gd
@@ -0,0 +1,6 @@
+extends "res://addons/gut/test.gd"
+
+class_name PlayerTest
+
+func test_simple_math():
+	assert_eq(2 + 2, 4)
